21c4c1918b1996e5cdb191ea41a71e12b670e22e
caishuxing <caishuxing@linggan.com>
2018-09-01 20:01:59 +0800

    修改大小写

--- app/src/main/java/com/komutr/client/event/LoginEvent.java
+++ app/src/main/java/com/komutr/client/event/LoginEvent.java
@@ -3,7 +3,7 @@ package com.komutr.client.event;
 import com.komutr.client.been.UserInfo;
 
 public class LoginEvent {
-    public static final int STATE_login_SUCCESS = 1;
+    public static final int STATE_LOGIN_SUCCESS = 1;
     public static final int STATE_LOGIN_OUT_SUCCESS = 2;
 
     private int stateType;
