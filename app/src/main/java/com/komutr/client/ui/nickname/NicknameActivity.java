2fb4abd8a13cc12aeda7a60fb192fedb6ff4eab7
caishuxing <caishuxing@linggan.com>
2018-09-01 21:00:56 +0800

    添加

---
+++ app/src/main/java/com/komutr/client/ui/nickname/NicknameActivity.java
@@ -0,0 +1,39 @@
+package com.komutr.client.ui.region;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.cai.framework.base.GodBasePresenter;
+import com.komutr.client.R;
+import com.komutr.client.base.App;
+import com.komutr.client.base.AppBaseActivity;
+import com.komutr.client.common.RouterManager;
+import com.komutr.client.databinding.RegionBinding;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+@Route(path = RouterManager.ROUTER_REGION, name = "我的-个人信息-地区")
+public class RegionActivity extends AppBaseActivity<RegionBinding> implements RegionView {
+    @Inject
+    RegionPresenter presenter;
+
+    @Override
+    public void initDagger() {
+        App.getAppComponent().inject(this);
+    }
+
+    @Override
+    public void addPresenters(List<GodBasePresenter> observerList) {
+        observerList.add(presenter);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.region;
+    }
+}
