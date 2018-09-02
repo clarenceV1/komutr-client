b83578b32b83a13ebf11b34e49b9dcc6515e9d09
gakes <491122011@qq.com>
2018-09-02 17:27:46 +0800

    对对对

--- app/src/main/java/com/komutr/client/common/Constant.java
+++ app/src/main/java/com/komutr/client/common/Constant.java
@@ -12,6 +12,8 @@ public class Constant {
     public static final String TEST_BASE_URL = "http://car.xinjuhao.wang/";
     private static final String IS_TEST_ENV = "is_test_env";//是否是测试环境
 
+    public static final String AUTH_KEY = "d511D54i5Odb6WT";
+
     /**
      * 设置是否是测试环境
      *
@@ -30,5 +32,23 @@ public class Constant {
     public static boolean isTestEnv() {
         SharedPreferences preferences = App.getAppContext().getSharedPreferences("default", Context.MODE_PRIVATE);
         return preferences.getBoolean(IS_TEST_ENV, false);
+    }
+
+
+    public interface RrequestFlag{
+
+
+        int FLAG_1 = 1;
+
+        int FLAG_2 = 2;
+
+        int FLAG_3 = 3;
+
+        int FLAG_4 = 4;
+
+        int FLAG_5 = 5;
+
+        int FLAG_6 = 6;
+
     }
 }
